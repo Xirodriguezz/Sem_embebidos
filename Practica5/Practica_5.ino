54b79edbe8f966a48d52062e57ba6031712405fa
Ximena Rodriguez <ximena.rodriguez4880@alumnos.udg.mx>
2025-04-01 17:58:11 -0600

    Update Practica_5.ino

--- Practica5/Practica_5.ino
+++ Practica5/Practica_5.ino
@@ -1,29 +1,25 @@
-const int ledPin = 2;    // Pin del LED
-const int ldrPin = 15;   // Pin de la fotoresistencia (LDR)
-int ldrValor = 0;        // Valor leído de la LDR
-int perc = 0;            // Porcentaje de luz
+const int pinPot = 15;  // Pin del potenciómetro
+const int LED = 13;      // Pin del LED
 
 void setup() {
-  Serial.begin(9600);    // Iniciar comunicación serial
-  delay(1000);           // Pequeño retardo inicial
+    Serial.begin(115200);
+    pinMode(LED, OUTPUT); 
 }
 
 void loop() {
-  ldrValor = analogRead(ldrPin); // Leer el valor de la LDR
+    int valorADC = analogRead(pinPot); // Leer el valor del potenciómetro
+    valorADC = (valorADC * 0.7) + (analogRead(pinPot) * 0.3); 
+    int brillo = map(valorADC, 0, 4095, 0, 255); // Convierte ADC a brillo (0-255)
+    int angulo = map(valorADC, 0, 4095, 0, 360); // Convierte ADC a ángulo
 
-  // Invertir el valor para que sea mayor en la oscuridad
-  ldrValor = 4095 - ldrValor;
+    analogWrite(LED, brillo); // Ajusta el brillo del LED
 
-  // Calcular el porcentaje de luz (opcional)
-  perc = (ldrValor * 100) / 4095;
+    Serial.print("Valor ADC: ");
+    Serial.print(valorADC);
+    Serial.print("  -  Brillo: ");
+    Serial.print(brillo);
+    Serial.print("  -  Ángulo: ");
+    Serial.println(angulo);
 
-  // Mostrar valores en el monitor serial
-  Serial.print("Valor LDR: " + String(ldrValor));
-  Serial.println("\tPorcentaje: " + String(perc));
-
-  // Ajustar el brillo del LED (0-255)
-  int brillo = map(ldrValor, 0, 4095, 0, 255);
-  analogWrite(ledPin, brillo);
-
-  delay(100); // Pequeño retardo para estabilizar la lectura
+//    delay(200); // 
 }
